f431a3d528c52727dce44608c47a69db300371ac
SUA <thekill.noobs7@gmail.com>
2026-03-26 19:33:13 -0400

    fix pt3

--- servidor/intermediarios/env.js
+++ servidor/intermediarios/env.js
@@ -1,6 +1,4 @@
-// Shim de variables de entorno compatible con Edge Runtime y ESLint.
-// En Edge Runtime, process.env es un global. No se puede importar node:process.
-/* eslint-disable node/prefer-global/process */
-import 'dotenv/config'
-
-export const env = globalThis.process?.env || globalThis.Bun?.env || {}
+export const env = {
+  ...(globalThis.process?.env || {}),
+  ...(globalThis.Bun?.env || {}),
+}
